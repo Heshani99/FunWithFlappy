adf406c6bae3a6af63e8e06a17490fc8055bfcdb
Heshani99 <heshaniwick99@gmail.com>
2024-06-20 01:23:19 +0530

    setting up flappy

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -15,6 +15,25 @@ public class FlappyBird extends JPanel{
     Image topPipeImg;
     Image bottomPipeImg;
 
+    //Bird
+    int birdX = boardWidth/8;
+    int birdY = boardHeight/2;
+    int birdWidth = 30;
+    int birdHeight = 24;
+
+    class Bird{
+        int x = birdX;
+        int y = birdY;
+        int width = birdWidth;
+        int height = birdHeight;
+        Image img;
+
+        Bird(Image img){
+            this.img = img;
+        }
+    }
+
+    Bird bird;
 
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth, boardHeight));
@@ -26,6 +45,9 @@ public class FlappyBird extends JPanel{
         topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
         bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
 
+        //bird
+        bird = new Bird(birdImg);
+
     }
 
     public void paintComponent(Graphics g){
@@ -36,5 +58,8 @@ public class FlappyBird extends JPanel{
     public void draw(Graphics g){
         //background
         g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+
+        //bird
+        g.drawImage(bird.img, bird.x, bird.y, bird.width, bird.height, null);
     }
 }
